dbb23f59357d30ad0535fce97d676a2626cce0d0
Patricia Arbona <arbonap@gmail.com>
2016-04-24 22:22:06 -0700

    Fraction Class

---
+++ Fraction_Class.py
@@ -0,0 +1,16 @@
+class Fraction:
+
+    def __init__(self, top, bottom):
+
+        self.num = top
+        self.den = bottom
+
+    def __str__(self):
+        return str(self.num) + "/" + str(self.den)
+
+    def __add__(self, otherfraction):
+        #cross multiply:
+        newnum = self.num * otherfraction.den + self.den * otherfraction.num
+        newden = self.den * otherfraction.den
+
+        return Fraction(newnum, newden)
